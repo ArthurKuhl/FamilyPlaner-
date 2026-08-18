3009add730eea3fa34f2e8c989a293711d0c3eee
ArthurKuhl <arth.kuhlmann@gmail.com>
2026-08-18 21:59:11 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Gartenplaner Service Worker
 // WICHTIG: CACHE_VERSION bei jedem Update der App-Datei erhöhen (z.B. 'v1' -> 'v2'),
 // sonst bekommen Nutzer weiterhin die alte, zwischengespeicherte Version ausgeliefert.
-const CACHE_VERSION = 'v95';
+const CACHE_VERSION = 'v96';
 const CACHE_NAME = 'planer-cache-' + CACHE_VERSION;
 
 const APP_SHELL = [
@@ -18,6 +18,7 @@ const APP_SHELL = [
   './haushalt.html',
   './geldgeschenke.html',
   './notizblock.html',
+  './ausfluege.html',
   './placeholder.html',
   './sync.js',
   './personen.js',
